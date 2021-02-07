7977135ae1bc14ab17f4f4ac090a45a02993c1c9
MCpracticesforskills <72710813+MCpracticesforskills@users.noreply.github.com>
2021-02-07 11:44:48 -0800

    changed css, html, and js
    added an h1 to the page. Added a farenheit and celcius converter in js

--- script.js
+++ script.js
@@ -51,9 +51,7 @@ function handleSubmit(event) {
 
 function searchLocation(position) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${
-    position.coords.latitude
-  }&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayWeatherCondition);
 }
@@ -85,4 +83,5 @@ searchForm.addEventListener("submit", handleSubmit);
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
-searchCity("New York");
+searchCity("Carthage");
+ 
